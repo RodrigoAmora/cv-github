171cb3b8ad3cef32f6731ec9c84f6fc6b333c8d7
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2018-05-07 02:33:03 -0300

    Showing message of error when the user wasn't find

--- src/app/make-cv-github/make-cv-github.component.ts
+++ src/app/make-cv-github/make-cv-github.component.ts
@@ -5,6 +5,7 @@ import { Http } from '@angular/http';
 import { BASE_API_GITHUB } from '../app.api'
 
 //MODELS
+import { ErrorGithub } from '../model/error-github';
 import { Owner } from '../model/owner';
 import { Repository } from '../model/repository';
 
@@ -15,49 +16,56 @@ import { Repository } from '../model/repository';
 })
 export class MakeCvGithubComponent implements OnInit {
 
+  errorGithub:ErrorGithub;
   owner:Owner;
   repositories: Repository[];
   
   name:String;
   newClient = new EventEmitter();
 
-  constructor(private http:Http) {
-    
-  }
+  constructor(private http:Http) {}
 
   ngOnInit() {}
 
   consultProfile() {
-    this.owner = new Owner;
-    this.repositories = [];
+    this.errorGithub = null;
+    this.owner = null;
+    this.repositories = null;
 
     var that = this;
     var stream = this.http.get(`${BASE_API_GITHUB}users/`+this.name+`/repos`);
-    stream.subscribe(function(res) {
-       for (var i = 0; i < res.json().length; i++) {
-         var repository = new Repository();
-         if (res.json()[i].description == null || res.json()[i].description == "") {
-          repository.description = "No description.";
-         } else {
-          repository.description = res.json()[i].description;
-         }
-         repository.full_name = res.json()[i].full_name;
-         repository.language = res.json()[i].language;
-         repository.name = res.json()[i].name;
-         repository.url = res.json()[i].html_url;
+    stream.subscribe((res) => {
+      that.owner = new Owner;
+      that.repositories = [];
+      for (var i = 0; i < res.json().length; i++) {
+        var repository = new Repository();
+        if (res.json()[i].description == null || res.json()[i].description == "") {
+        repository.description = "No description.";
+        } else {
+        repository.description = res.json()[i].description;
+        }
+        repository.full_name = res.json()[i].full_name;
+        repository.language = res.json()[i].language;
+        repository.name = res.json()[i].name;
+        repository.url = res.json()[i].html_url;
 
-         that.repositories.push(repository);
-       }
+        that.repositories.push(repository);
+      }
 
-       that.owner.avatar_url = res.json()[0]['owner']['avatar_url'];
-       //that.owner.gists_url = res.json()[0]['owner']['gists_url'];
-       that.owner.gists_url = "https://gist.github.com/"+that.name;
-       that.owner.html_url = res.json()[0]['owner']['html_url'];
-       that.owner.id = res.json()[0]['owner']['id'];
-       that.owner.login = res.json()[0]['owner']['login'];
-       that.owner.repositories = that.repositories;
-       that.owner.getTotalRepositories();
+      that.owner.avatar_url = res.json()[0]['owner']['avatar_url'];
+      //that.owner.gists_url = res.json()[0]['owner']['gists_url'];
+      that.owner.gists_url = "https://gist.github.com/"+that.name;
+      that.owner.html_url = res.json()[0]['owner']['html_url'];
+      that.owner.id = res.json()[0]['owner']['id'];
+      that.owner.login = res.json()[0]['owner']['login'];
+      that.owner.repositories = that.repositories;
+      that.owner.getTotalRepositories();
+    }, error => {
+      that.errorGithub = new ErrorGithub;
+      that.errorGithub.message = "Error!";
+      console.log(error.json());
     });
+
     this.newClient.emit(this.owner);
   }
 
---
+++ src/app/model/error-github.ts
@@ -0,0 +1,5 @@
+export class ErrorGithub {
+    
+    public message:String;
+    
+}
