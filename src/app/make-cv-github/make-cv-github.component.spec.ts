ba4c8a5265883d835b14486423729065a6f437a9
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2018-07-29 00:14:21 -0300

    Adjusts

--- src/app/make-cv-github/make-cv-github.component.spec.ts
+++ src/app/make-cv-github/make-cv-github.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MakeCvGithubComponent } from './make-cv-github.component';
 
 describe('MakeCvGithubComponent', () => {
